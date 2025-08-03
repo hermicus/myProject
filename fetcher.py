543d29cd5011783fc5f080165ccef4d6213cc43b
Melki <melki.vatansever@gmail.com>
2025-08-03 22:13:03 +0200

    First changes

--- fetcher.py
+++ fetcher.py
@@ -1,14 +1,21 @@
 import yfinance as yf
 import sqlite3
+from datetime import datetime
 
-conn = sqlite3.connect('mytable1.sqlite')
+conn = sqlite3.connect('mysql1.sqlite')
 cur = conn.cursor()
+#cur.execute('ALTER TABLE stocks ADD basis_currency_id integer;')
 
-cur.execute('DROP TABLE IF EXISTS Date')
+conn.commit()
+
+temp_list = cur.execute('SELECT ticker FROM stocks')
+for row in temp_list:
+    dat = yf.Ticker(str(row[0]))
+    splitData = dat.history(start="2025-01-01", end="2025-12-31")
+    splitData = splitData.to_json()
+    print(splitData)
+    #print(dir(dat.history()))
 
 
-
-dat = yf.Ticker("RYSAS.IS")
-splitData = dat.history(start="2025-01-01", end="2025-12-31").to_dict()
-
+cur.close()
 
