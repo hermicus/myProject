3e8428bc9f38aafc6a72848c4437e0e4e481d2c7
Melki <melki.vatansever@gmail.com>
2025-08-05 21:34:45 +0200

    First changes

--- fetcher.py
+++ fetcher.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 import sqlite3
-from datetime import datetime
+import json
+
+from websockets import Close
 
 conn = sqlite3.connect('mysql1.sqlite')
 cur = conn.cursor()
@@ -11,11 +13,15 @@ conn.commit()
 temp_list = cur.execute('SELECT ticker FROM stocks')
 for row in temp_list:
     dat = yf.Ticker(str(row[0]))
-    splitData = dat.history(start="2025-01-01", end="2025-12-31")
-    splitData = splitData.to_json()
-    print(splitData)
-    #print(dir(dat.history()))
+    fname = dat.history(start="2025-01-01", end="2025-01-31")
+    strData = fname.to_json()
+    jsonData = json.loads(strData)
+    for entry in jsonData.items()["Close"]:
+        CloseData = entry[0:]
+        print(CloseData)
 
 
 cur.close()
 
+# Changing date to list and then adding to DB + whiling through the values
+# Or somehow using items to update SQL ?
