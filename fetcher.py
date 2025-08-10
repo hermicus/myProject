c6b8a1cfd7a6f546bedd80b6b3a9ec0498aa2ead
Melki <melki.vatansever@gmail.com>
2025-08-10 22:21:09 +0200

    First changes

--- fetcher.py
+++ fetcher.py
@@ -4,11 +4,45 @@ import json
 
 from websockets import Close
 
+#Dumping and creating fresh tables for testing purposes
+
 conn = sqlite3.connect('mysql1.sqlite')
 cur = conn.cursor()
-#cur.execute('ALTER TABLE stocks ADD basis_currency_id integer;')
+cur.executescript('''
+DROP TABLE IF EXISTS stocks;
+DROP TABLE IF EXISTS currency;
+DROP TABLE IF EXISTS prices;
+
+CREATE TABLE "currency" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"Currency"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE "stocks" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"ticker"	TEXT NOT NULL,
+	"basis_currency_id"	integer NOT NULL,
+	PRIMARY KEY("id")
+);
+
+CREATE TABLE "prices" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"date"	INTEGER NOT NULL,
+	"currency_id"	INTEGER NOT NULL,
+	"stock_id"	INTEGER NOT NULL,
+	"close_price"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+''')
 
 conn.commit()
+
+#Adding base data for the fetching
+
+cur.execute('''INSERT OR IGNORE INTO User (name)
+        VALUES ( ? )''', ( name, ) )
+
 
 temp_list = cur.execute('SELECT ticker FROM stocks')
 for row in temp_list:
@@ -20,6 +54,7 @@ for row in temp_list:
         CloseData = entry[0:]
         print(CloseData)
 
+conn.commit()
 
 cur.close()
 
