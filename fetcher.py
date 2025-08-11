de0c31c30b72685ceb8b2d5f8d312a9ec888c0a8
Melki <melki.vatansever@gmail.com>
2025-08-11 22:17:52 +0200

    installed crontab + added to requirements.txt

--- fetcher.py
+++ fetcher.py
@@ -2,11 +2,12 @@ import yfinance as yf
 import sqlite3
 import json
 import datetime
+from crontab import CronTab
 
 from websockets import Close
 
 #Dumping and creating fresh tables for testing purposes
-
+today = datetime.datetime.today().date()
 conn = sqlite3.connect('mysql1.sqlite')
 cur = conn.cursor()
 cur.executescript('''
@@ -36,16 +37,17 @@ CREATE TABLE "prices" (
 ''')
 
 conn.commit()
-today = datetime.datetime.today().date()
+
 #Adding base data for the fetching
 
-cur.executescript('''INSERT OR IGNORE INTO stocks ( ticker ) VALUES ("RYSAS.IS"),("RYGYO.IS");
+cur.executescript('''INSERT OR IGNORE INTO stocks ( ticker ) VALUES ("RYSAS.IS"),("RYGYO.IS"), ("AGESA.IS");
 
 INSERT OR IGNORE INTO currency (currency) VALUES ("TRY");
 ''')
 
 conn.commit()
 
+#Load all the tickers in de DB to a list
 
 temp_list = cur.execute('SELECT ticker, id FROM stocks').fetchall()
 for row in temp_list:
@@ -55,14 +57,15 @@ for row in temp_list:
 	rawData = fname.to_json()
 	jsonData = json.loads(rawData)
 
-	#cur.execute('SELECT id FROM stocks where ticker = ?', (temp_list[0], ))
+	#Fetch all adjusted closing date and insert them one by one into de DB
 
 	for key, value in jsonData["Adj Close"].items():
 		sql = "INSERT or IGNORE INTO prices (close_price, date, stock_id) VALUES (?, ?, ?)"
 		cur.execute(sql, (value, key, stock_id))
-		conn.commit()
+conn.commit()
 
 cur.close()
 
-#def my_converter(ticker, target_curency)
+def my_updater():
+	ticker_to_update = cur.execute('SELECT ticker, id FROM stocks').fetchall()
 
