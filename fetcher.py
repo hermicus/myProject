800efc84a38d36fe883310702fe2ffd53a1bb707
Melki <melki.vatansever@gmail.com>
2025-08-14 18:03:06 +0200

    Conversion of syntax to mysql/python

--- fetcher.py
+++ fetcher.py
@@ -1,54 +1,43 @@
 import yfinance as yf
-import sqlite3
+import mysql.connector
 import json
 import datetime
 
-from websockets import Close
+conn = mysql.connector.connect(
+host="localhost",
+user="admin",
+password="admin",
+database="db_name"
+)
 
 #Dumping and creating fresh tables for testing purposes
 today = datetime.datetime.today().date()
-conn = sqlite3.connect('mysql1.sqlite')
+
 cur = conn.cursor()
-cur.executescript('''
-DROP TABLE IF EXISTS stocks;
-DROP TABLE IF EXISTS currency;
-DROP TABLE IF EXISTS prices;
 
-CREATE TABLE "currency" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"currency"	TEXT NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);
 
-CREATE TABLE "stocks" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"ticker"	TEXT NOT NULL Unique,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);
 
-CREATE TABLE "prices" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"date"	INTEGER NOT NULL,
-	"stock_id"	INTEGER NOT NULL,
-	"close_price"	INTEGER NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);
-''')
+# Drop tables
+cur.execute('DROP TABLE IF EXISTS prices')
+cur.execute('DROP TABLE IF EXISTS stocks')
+cur.execute('DROP TABLE IF EXISTS currency')
+
+# Create tables
+cur.execute('CREATE TABLE currency (id INT AUTO_INCREMENT PRIMARY KEY, currency VARCHAR(3))')
+cur.execute('CREATE TABLE stocks (id INT UNIQUE AUTO_INCREMENT PRIMARY KEY, ticker VARCHAR(100) UNIQUE)')
+cur.execute('CREATE TABLE prices (id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, date INT NOT NULL, stock_id INT NOT NULL, close_price DECIMAL(20,6) NOT NULL)')
+
+# Insert base data
+cur.execute('INSERT IGNORE INTO stocks (ticker) VALUES ("RYSAS.IS"), ("RYGYO.IS"), ("AGESA.IS")')
+cur.execute('INSERT IGNORE INTO currency (currency) VALUES ("TRY")')
 
 conn.commit()
-
-#Adding base data for the fetching
-
-cur.executescript('''INSERT OR IGNORE INTO stocks ( ticker ) VALUES ("RYSAS.IS"),("RYGYO.IS"), ("AGESA.IS");
-
-INSERT OR IGNORE INTO currency (currency) VALUES ("TRY");
-''')
-
-conn.commit()
+cur.reset()
 
 #Load all the tickers in de DB to a list
 
-temp_list = cur.execute('SELECT ticker, id FROM stocks').fetchall()
+cur.execute('SELECT ticker, id FROM stocks')
+temp_list = cur.fetchall()
 for row in temp_list:
 	stock_id = row[1]
 	dat = yf.Ticker(str(row[0]))
@@ -59,11 +48,13 @@ for row in temp_list:
 	#Fetch all adjusted closing date and insert them one by one into de DB
 
 	for key, value in jsonData["Adj Close"].items():
-		sql = "INSERT or IGNORE INTO prices (close_price, date, stock_id) VALUES (?, ?, ?)"
-		cur.execute(sql, (value, key, stock_id))
-conn.commit()
+		sql = "INSERT IGNORE INTO prices (close_price, date, stock_id) VALUES (%s, %s, %s)"
+		cur.execute(sql, (value, int(key), stock_id))
+		conn.commit()
+
 
 cur.close()
+conn.close()
 
 def my_updater():
 	ticker_to_update = cur.execute('SELECT ticker, id FROM stocks').fetchall()
